b0b568e9df3a70644912a7f0fbad660eb90f072b
kikosaa1998 <135472258+kikosaa1998@users.noreply.github.com>
2023-12-24 22:11:51 +0200

    nnnm

---
+++ wp-content/themes/epaper-news/assets/js/epaper-news-main.js
@@ -0,0 +1,11 @@
+(function ($) {
+    "use strict";
+    //Document ready function
+    jQuery(document).ready(function($){
+        //Masonry ===================================
+        $(".menu-close").focus(function() {
+            $("a.slicknav_open").focus();
+        });
+    });//End document ready function 
+
+}(jQuery)); 
